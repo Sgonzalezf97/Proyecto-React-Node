c8444c810273b4fecb58a6035df17b4e6574121f
Juan Sebastian Gonzalez Forero <72105720+Sgonzalezf97@users.noreply.github.com>
2022-12-08 22:36:56 -0500

    Control de peticiones por distintos metodos

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,22 +1,70 @@
-
+// se importa la conexión a la base de datos
+const pool = require('../db')
+//Hace un select a todas las tareas existentes y las devuelve en un json (por medio del metodo get)
 const getAllTasks = async (req, res) => {
-    res.send('retornando la lista de tareas')
+    try {
+        const allTasks = await pool.query("SELECT * FROM task " );
+        res.json(allTasks.rows)
+    } catch (error) {
+        res.json({error: error.message});
+    }
 }
-
+// hace un select a una sola tarea dando el id, devolviendo los datos de la tarea en un json, en caso de no existir envia un error (por medio del metodo get)
 const getTask = async (req, res) => {
-    res.send('retornando la tarea 10')
+   try {
+    const {id}= req.params
+    const result = await pool.query("SELECT * FROM task WHERE id = $1 ",[id])
+    console.log(result)
+    if (result.rows.length === 0)
+        return res.status(404).json({
+            message : 'Tarea no encontrada'
+        })
+    res.json(result.rows[0]);
+   } catch (error) {
+    console.log(message.error);
+   }
 }
+//Crea una tarea pasandole los parametros por medio de un json (por medio del metodo post )
+const createTask = async (req, res) => {
+    const {title,description} = req.body
 
-const createTask = (req, res) => {
-    res.send('creando la lista de tareas')
+    try {
+        const result = await pool.query("INSERT INTO task (title, description) VALUES ($1,$2) RETURNING *",[
+            title,
+            description
+        ])
+        //console.log(result)
+        res.json(result.rows[0])
+    } catch (error) {
+        res.json({error: error.message});
+    }
 }
-
-const deleteTask = (req, res) => {
-    res.send('eliminando la lista de tareas')
+//Elimina una tarea pasandole el parametro del id 
+const deleteTask = async (req, res) => {
+    const {id}= req.params
+    console.log(req.params.id)
+    const result = await pool.query("DELETE FROM task WHERE id = $1 RETURNING *",[id])
+    console.log(result)
+    if (result.rowCount === 0)
+        return res.status(404).json({
+            message: "No se ha eliminado la tarea"
+        });
+    console.log("se ha eliminado la tarea correctamente");
+    return res.sendStatus(204);
+    
 }
+//Actualiza una tarea recibiendo los nuevos parametros
+const updateTask = async(req, res) => {
+    const {id}= req.params;
+    const {title,description} = req.body;
 
-const updateTask = (req, res) => {
-    res.send('Actualizando la lista de tareas')
+    const result = await pool.query("UPDATE task SET title = $1,description = $2 WHERE id = $3 RETURNING *",[title,description,id]);
+    if (result.rows.length === 0)
+        return res.status(404).json({
+            message: "Tarea no encontrada",
+        });
+
+    return res.json (result.rows[0])
 }
 
 module.exports = {
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,22 +1,22 @@
+//se importa el framwork express
 const {Router} = require('express');
+//se importa la conexión a la base de datos
 const pool =require('../db');
+// se importan todas los controladores para realizar operaciones CRUD
 const {getAllTasks, getTask, createTask, deleteTask, updateTask } = require('../controllers/tasks.controller')
 
-
+//se instancia para poder usarlo y hacer las peticiones
 const router = Router();
-
+//metodo get para traer todoas las tareas
 router.get('/tasks', getAllTasks)
-    //const result = await pool.query('SELECT NOW()')
-    //console.log(result)
-    //res.json(result.rows[0].now)
-
-router.get('/tasks/10', getTask)
-
+//Metodo get para traer una unica tarea por medio de un id
+router.get('/tasks/:id', getTask)
+//Metodo post para crear una tarea
 router.post('/tasks', createTask)
+//Metodo 
+router.delete('/tasks/:id',deleteTask)
 
-router.delete('/tasks',deleteTask)
-
-router.put('/tasks', updateTask)
+router.put('/tasks/:id', updateTask)
 
 
 module.exports = router;
